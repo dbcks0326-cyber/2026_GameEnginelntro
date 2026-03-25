b7877615ba9ad15c41bcdb570f70f2a9fdd8c73a
hyn335 <hyn335@Naver.com>
2026-03-25 16:46:21 +0900

    ui 만들기

--- 게임엔진입문/Assets/PLAYERCONTROLLER.cs
+++ 게임엔진입문/Assets/PLAYERCONTROLLER.cs
@@ -1,33 +1,32 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using UnityEngine.SceneManagement;
 public class PLAYERCONTROLLER : MonoBehaviour
 {
     private Vector2 moveInput;
-    public float moveSpeed = 50f;
-    public float jumpForce = 60f;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
     private Rigidbody2D rb;
     private Animator myAnimator;
 
 
-
     void Start()
-    
+
     {
         rb = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
         myAnimator.SetBool("moce", false);
     }
     public void OnMove(InputValue value)
-    { moveInput = value.Get<Vector2>();  }
+    { moveInput = value.Get<Vector2>(); }
 
     public void OnJump(InputValue value)
     {
         if (value.isPressed)
         { rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); }
     }
-   
+
     // Update is called once per frame
     void Update()
     {
@@ -54,5 +53,17 @@ public class PLAYERCONTROLLER : MonoBehaviour
     }
 
    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+     if (collision.name == "Death")
+    {
+          SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+     else
+    {
+          SceneManager.LoadScene("PlayScene_" + collision.name);
+    }
+    }
+    }
+            
 
-}
